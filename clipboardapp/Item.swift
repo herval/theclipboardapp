86de867cea04f754dd1ed104f521c2ecc270a9fc
herval <hervalfreire@gmail.com>
2025-07-05 12:38:02 -0400

    Initial Commit

---
+++ clipboardapp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  clipboardapp
+//
+//  Created by herval on 7/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ clipboardappTests/clipboardappTests.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardappTests.swift
+//  clipboardappTests
+//
+//  Created by herval on 7/5/25.
+//
+
+import Testing
+@testable import clipboardapp
+
+struct clipboardappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
